81ec67ca6e7bdcd1213d718bd668fb02c4092882
Gilberto Lobo <gvampre1991@gmail.com>
2021-03-17 20:04:39 -0300

    first commit, playing with mod

---
+++ src/github.com/GilbertoVGL/exercises/exercise_00/exercise_00.go
@@ -0,0 +1,7 @@
+package exercise_00
+
+import "rsc.io/quote"
+
+func Hello() string {
+	return quote.Hello()
+}
---
+++ src/github.com/GilbertoVGL/exercises/exercise_00/exercise_00_test.go
@@ -0,0 +1,10 @@
+package exercise_00
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	want := "Hello, world."
+	if got := Hello(); got != want {
+		t.Errorf("Hello() = %q, want %q", got, want)
+	}
+}
